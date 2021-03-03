6689f45148bc44b309e0fc96b84642f22a44f8fd
Nikolas Doğan Halil <nikolasdogan@gmail.com>
2021-03-03 15:17:30 +0300

    Create unsuccesfull.php

---
+++ unsuccesfull.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+* $_GET['message'] = Hata mesajı;
+* $_GET['code']    = Hata kodu;
+**/
+session_start();
+if(isset($_GET['message'])){
+	echo ($_GET['message']);
+	
+} else{
+	echo "Banka parayı çekemedi.No otorozizasyon.";
+}
+?>
